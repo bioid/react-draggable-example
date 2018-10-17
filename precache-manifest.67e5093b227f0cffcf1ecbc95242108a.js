1efb27bf2707c03e9645bf359d41630910a85e1d
Brandon Hinshaw <bkhinshaw@gmail.com>
2018-10-16 17:55:51 -0700

    Updates

---
+++ precache-manifest.67e5093b227f0cffcf1ecbc95242108a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-draggable-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7193923e929aafbd66d",
+    "url": "/react-draggable-example/static/js/runtime~main.d7193923.js"
+  },
+  {
+    "revision": "18f1034fb0e4e411baf3",
+    "url": "/react-draggable-example/static/js/main.18f1034f.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-draggable-example/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "18f1034fb0e4e411baf3",
+    "url": "/react-draggable-example/static/css/main.07ae30d4.chunk.css"
+  },
+  {
+    "revision": "bc7cdc155f76f218d0f87a1ec601e601",
+    "url": "/react-draggable-example/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e49203d44aab8826d4ed1ab14a4915e.js"
+  "/react-draggable-example/precache-manifest.67e5093b227f0cffcf1ecbc95242108a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-draggable-example/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
